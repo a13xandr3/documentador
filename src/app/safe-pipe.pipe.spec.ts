0a5bfed73fa7e2aa8df1b95a90f8e18110815daa
Alexandre Esteves <a13xandr3ea@gmail.com>
2024-10-17 15:52:38 -0300

    formatação modal

--- src/app/safe-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SafePipePipe } from './safe.pipe';
-
-describe('SafePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/safe.pipe.ts
+++ src/app/safe.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Pipe({
   name: 'safe'
@@ -7,6 +7,7 @@ import {DomSanitizer} from "@angular/platform-browser";
 export class SafePipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer) { }
+  
   transform(url: any) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
